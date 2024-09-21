d602d1a9c8720544fc100ca32d2f876e55c4131f
Eoin Lane <eoinlane@gmail.com>
2024-09-21 19:10:37 +0100

    Changed theme from paradise to jellybeans

--- config/default.nix
+++ config/default.nix
@@ -66,7 +66,7 @@
   ];
   options = {
     theme = lib.mkOption {
-      default = lib.mkDefault "paradise";
+      default = lib.mkDefault "jellybeans";
       type = lib.types.enum [
         "paradise"
         "decay"
@@ -90,7 +90,7 @@
   };
   config = {
     # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
-    theme = "paradise";
+    theme = "jellybeans";
     extraConfigLua = ''
       _G.theme = "${config.theme}"
     '';
